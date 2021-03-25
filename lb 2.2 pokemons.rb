a39d8ba70ffccf999cb08f923f8bd70d9414e091
skazkavostoka <77774968+skazkavostoka@users.noreply.github.com>
2021-03-25 12:12:35 +0300

    Add files via upload

---
+++ lb 2.2 pokemons.rb
@@ -0,0 +1,29 @@
+def pokemons(name*i)
+
+def printpok(name, color)
+	puts "Name: #{name}"
+	puts "Color: #{color}"
+end
+
+print "name pokemon"
+name = gets
+print "color pokemon"
+color = gets
+
+
+
+pokemons = []
+
+def pokemonas(name* , color*)
+
+count = nil
+
+puts "How many pokemons you wish add?"
+count = gets.to_i
+ for i in 0.....count-1
+ 	puts "pokemon #{i+1}"
+ 	puts  printpok
+
+
+
+
